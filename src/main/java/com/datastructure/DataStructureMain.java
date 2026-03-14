209b04842309c45b252c4cdcb356af0f9222011d
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:47:01 +0530

    UC2: Operations

--- src/main/java/com/datastructure/DataStructureMain.java
+++ src/main/java/com/datastructure/DataStructureMain.java
@@ -3,14 +3,14 @@ package com.datastructure;
 import java.util.Scanner;
 
 /**
- * UC1 - then 56 to make 56 on top of the Stack
+ * UC2 - Operations
  *
  * Requirements:
-     *   - Ability to peak and
-     *   - pop from the Stack
-     *   - till it is empty
+     *   - Queue API
+     *   - Ability to create a Queue of
      *   - 56->30->70
-     *   - Use LinkedList to do the Stack
+     *   - Use LinkedList to do the Queue
+     *   - Operations
  *
  * @author  VignejanBridgeLabz
  * @version 1.0
@@ -20,14 +20,10 @@ public class DataStructureMain {
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure");
         
-        System.out.println("\n=== UC1: then 56 to make 56 on top of the Stack ===");
-        // Stack using LinkedList (LIFO)
-        java.util.LinkedList<Integer> stack = new java.util.LinkedList<>();
-        stack.add(70); stack.add(30); stack.add(56); // push order
-        System.out.println("Stack created: " + stack);
-        // Peek and Pop
-        while (!stack.isEmpty()) {
-            System.out.println("Peek: " + stack.getFirst() + " | Pop: " + stack.removeFirst());
-        }
+        System.out.println("\n=== UC2: Operations ===");
+        // TODO: Implement UC2
+        // 
+Queue API        // Ability to create a Queue of        // 56->30->70
+        System.out.println("UC2 - Implementation pending");
     }
 }

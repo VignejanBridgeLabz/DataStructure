2d7ad5398176a0e48ceb81da6b18c7799a733549
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:43:51 +0530

    UC3: - LinkedList Sequence: 56->30->70

--- src/main/java/com/datastructure/DataStructureMain.java
+++ src/main/java/com/datastructure/DataStructureMain.java
@@ -3,14 +3,13 @@ package com.datastructure;
 import java.util.Scanner;
 
 /**
- * UC2 - - LinkedList Sequence: 56->30->70
+ * UC3 - - LinkedList Sequence: 56->30->70
  *
  * Requirements:
-     *   - Ability to create Linked
-     *   - List by appending 30 and
-     *   - 70 to 56
-     *   - Node with data 56 is First Created
-     *   - Next Append 30 to 56
+     *   - Ability to insert
+     *   - 30 between 56
+     *   - and 70
+     *   - Final Sequence: 56->30->70
  *
  * @author  VignejanBridgeLabz
  * @version 1.0
@@ -20,12 +19,12 @@ public class DataStructureMain {
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure");
         
-        System.out.println("\n=== UC2: - LinkedList Sequence: 56->30->70 ===");
+        System.out.println("\n=== UC3: - LinkedList Sequence: 56->30->70 ===");
         // LinkedList operations
         java.util.LinkedList<Integer> list = new java.util.LinkedList<>();
         list.add(56); list.add(30); list.add(70);
         System.out.println("LinkedList: " + list);
-        // TODO: Implement specific LinkedList operation for UC2
-        // Ability to create Linked; List by appending 30 and
+        // TODO: Implement specific LinkedList operation for UC3
+        // Ability to insert; 30 between 56
     }
 }

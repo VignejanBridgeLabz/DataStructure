637ff7393cbbd733cd0ed86e3c6da2700baf2fb2
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:47:06 +0530

    UC3: then 70 to make 56 on top of the Stack

--- src/main/java/com/datastructure/DataStructureMain.java
+++ src/main/java/com/datastructure/DataStructureMain.java
@@ -3,14 +3,13 @@ package com.datastructure;
 import java.util.Scanner;
 
 /**
- * UC2 - Operations
+ * UC3 - then 70 to make 56 on top of the Stack
  *
  * Requirements:
-     *   - Queue API
-     *   - Ability to create a Queue of
-     *   - 56->30->70
-     *   - Use LinkedList to do the Queue
-     *   - Operations
+     *   - Ability to dequeue
+     *   - from the beginning
+     *   - Use LinkedList to do the
+     *   - Queue Operations
  *
  * @author  VignejanBridgeLabz
  * @version 1.0
@@ -20,10 +19,14 @@ public class DataStructureMain {
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure");
         
-        System.out.println("\n=== UC2: Operations ===");
-        // TODO: Implement UC2
-        // 
-Queue API        // Ability to create a Queue of        // 56->30->70
-        System.out.println("UC2 - Implementation pending");
+        System.out.println("\n=== UC3: then 70 to make 56 on top of the Stack ===");
+        // Stack using LinkedList (LIFO)
+        java.util.LinkedList<Integer> stack = new java.util.LinkedList<>();
+        stack.add(70); stack.add(30); stack.add(56); // push order
+        System.out.println("Stack created: " + stack);
+        // Peek and Pop
+        while (!stack.isEmpty()) {
+            System.out.println("Peek: " + stack.getFirst() + " | Pop: " + stack.removeFirst());
+        }
     }
 }

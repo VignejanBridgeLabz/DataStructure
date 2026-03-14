9f4cb7ad3d032e808ab04dc53f64014c08be47c5
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:44:17 +0530

    UC7: find node with key 30

--- src/main/java/com/datastructure/DataStructureMain.java
+++ src/main/java/com/datastructure/DataStructureMain.java
@@ -3,14 +3,14 @@ package com.datastructure;
 import java.util.Scanner;
 
 /**
- * UC6 - Ability to search
+ * UC7 - find node with key 30
  *
  * Requirements:
-     *   - LinkedList to find Node
-     *   - with value 30
-     *   - Write Junit Test Case as
-     *   - demonstrated in class
-     *   - Loop through LinkedList to
+     *   - Ability to insert 40 after 30 to
+     *   - the Linked List sequence of
+     *   - 56->30->70
+     *   - Write Junit Test Case as demonstrated
+     *   - in class
  *
  * @author  VignejanBridgeLabz
  * @version 1.0
@@ -20,10 +20,12 @@ public class DataStructureMain {
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure");
         
-        System.out.println("\n=== UC6: Ability to search ===");
-        // TODO: Implement UC6
-        // 
-LinkedList to find Node        // with value 30        // Write Junit Test Case as
-        System.out.println("UC6 - Implementation pending");
+        System.out.println("\n=== UC7: find node with key 30 ===");
+        // LinkedList operations
+        java.util.LinkedList<Integer> list = new java.util.LinkedList<>();
+        list.add(56); list.add(30); list.add(70);
+        System.out.println("LinkedList: " + list);
+        // TODO: Implement specific LinkedList operation for UC7
+        // Ability to insert 40 after 30 to; the Linked List sequence of
     }
 }

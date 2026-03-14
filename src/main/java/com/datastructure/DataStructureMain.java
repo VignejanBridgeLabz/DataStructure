1b686b830ff34e07fdbd4e90733b5788461a489f
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:46:40 +0530

    START: Display welcome message in DataStructureMain

--- src/main/java/com/datastructure/DataStructureMain.java
+++ src/main/java/com/datastructure/DataStructureMain.java
@@ -1,26 +1,7 @@
 package com.datastructure;
 
-import java.util.Scanner;
-
-/**
- * UC10 - - Perform Sorting during the add method call
- *
- * Requirements:
-     *   - Final Sequence: 30->40->56->70
-     *   - Ordered
-     *   - LinkedList
-     *   - API
- *
- * @author  VignejanBridgeLabz
- * @version 1.0
- */
 public class DataStructureMain {
-
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure");
-        
-        System.out.println("\n=== UC10: - Perform Sorting during the add method call ===");
-        // TODO: Implement sort logic - ascending/alphabetical order
-        System.out.println("Sorted successfully!");
     }
 }

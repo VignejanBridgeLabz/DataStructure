f26dea379232249d77e86fa0481251a9e2eff380
VignejanBridgeLabz <vignejan.sivanesan@bridgelabz.com>
2026-03-14 14:44:22 +0530

    UC8: - Then Insert 40 to 30

--- src/main/java/com/datastructure/DataStructureMain.java
+++ src/main/java/com/datastructure/DataStructureMain.java
@@ -3,14 +3,14 @@ package com.datastructure;
 import java.util.Scanner;
 
 /**
- * UC7 - find node with key 30
+ * UC8 - - Then Insert 40 to 30
  *
  * Requirements:
-     *   - Ability to insert 40 after 30 to
-     *   - the Linked List sequence of
-     *   - 56->30->70
-     *   - Write Junit Test Case as demonstrated
-     *   - in class
+     *   - Final Sequence: 56->30->40->70
+     *   - Ability to delete 40 from the Linked
+     *   - List sequence of 56->30->40->70
+     *   - and show the size of LinkedList is 3
+     *   - Write Junit Test Case as demonstrated in class
  *
  * @author  VignejanBridgeLabz
  * @version 1.0
@@ -20,12 +20,12 @@ public class DataStructureMain {
     public static void main(String[] args) {
         System.out.println("Welcome to DataStructure");
         
-        System.out.println("\n=== UC7: find node with key 30 ===");
+        System.out.println("\n=== UC8: - Then Insert 40 to 30 ===");
         // LinkedList operations
         java.util.LinkedList<Integer> list = new java.util.LinkedList<>();
         list.add(56); list.add(30); list.add(70);
         System.out.println("LinkedList: " + list);
-        // TODO: Implement specific LinkedList operation for UC7
-        // Ability to insert 40 after 30 to; the Linked List sequence of
+        // TODO: Implement specific LinkedList operation for UC8
+        // Final Sequence: 56->30->40->70; Ability to delete 40 from the Linked
     }
 }
